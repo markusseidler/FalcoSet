b11b1eba7808291055a0febcb6acb211272358fe
Markus Seidler <mseidler79@gmail.com>
2020-08-15 22:39:10 +0800

    Initial Commit

---
+++ FalcoSet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FalcoSet
+//
+//  Created by Markus Seidler on 15/8/20.
+//  Copyright © 2020 Mata. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
